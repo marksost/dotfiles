441ff3ce641785f5a6f417b0f4dc715191f3068b
Mark Sost <sostmark@gmail.com>
2018-05-21 11:47:44 -0400

    Brew updates

--- cli.sh
+++ cli.sh
@@ -20,55 +20,19 @@ brew install gpg
 # Install RVM before installing other binaries
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable --ruby
-rvm install 2.3.0
-rvm use 2.3.0
+rvm install 2.4.1
+rvm use 2.4.1
 
-# Install more recent versions of some macOS tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install wget
-
-# Install PHP and utilities
-brew install homebrew/php/php70
-brew install homebrew/php/php70-xdebug
-brew install homebrew/php/phplint
-brew install composer
-
-# Install standard binaries
-brew install node
-brew install go
-brew install ansible
-brew install fleetctl
-brew install jq
-brew install redis
-brew install dep
-
-# Install IT tools
-brew install pv
-brew install siege
-brew install terraform
-
-# Install k8s tools
-brew install kubectl
-brew install kubernetes-helm
-
-(
-  cd /tmp
-  curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/kubernetes-client-darwin-amd64.tar.gz
-  tar -xzvf kubernetes-client-darwin-amd64.tar.gz
-  sudo cp kubernetes/client/bin/kubefed /usr/local/bin
-  sudo chmod +x /usr/local/bin/kubefed
-)
-
-# Install MySQL
-brew install mysql
+# Install brew packages
+for FORMULA in $(cat "./brew/formula-list.txt"); do
+  brew install "${FORMULA}"
+done
 
 # Install NPM packages
 npm install -g eslint jshint bower grunt-cli
 
 # Install gems
+gem install github_changelog_generator
 gem install travis
 
 # Ensure directories are writable
@@ -82,11 +46,6 @@ python get-pip.py
 # Install PIP utilities
 pip install --upgrade pip setuptools --ignore-installed six
 pip install awscli virtualenv virtualenvwrapper --ignore-installed six
-
-# Install Deis utilities
-cd /usr/local/bin/
-curl -sSL http://deis.io/deisctl/install.sh | sh -s 1.13.3
-curl -sSL http://deis.io/deis-cli/install.sh | sh
 
 # Install GCP tools
 # NOTE: Interractive
