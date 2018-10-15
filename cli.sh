c789b59ad16ae26a0599eef43d3ae6c3c73ac9b2
Mark Sost <sostmark@gmail.com>
2018-10-15 10:58:21 -0400

    Apt packages

--- cli.sh
+++ cli.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
 
-# Install RVM before installing other binaries
-# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-# curl -sSL https://get.rvm.io | bash -s stable --ruby
-# rvm install 2.4.1
-# rvm use 2.4.1
-
 # Install apt packages
-# apt install -y $(grep -vE "^\s*#" "./apt/packages.txt"  | tr "\n" " ")
+sudo apt update
+sudo apt install -y $(grep -vE "^\s*#" "./apt/packages.txt"  | tr "\n" " ")
 
 # Install NPM packages
-# npm install -g eslint jshint bower grunt-cli
+npm install -g eslint jshint
 
 # Install gems
-# gem install github_changelog_generator
-# gem install travis
-
-# Get and install PIP
-# cd /tmp
-# curl -O https://bootstrap.pypa.io/get-pip.py
-# python get-pip.py
+gem install github_changelog_generator
+gem install travis
 
 # Install PIP utilities
-# pip install --upgrade pip setuptools
-# pip install awscli virtualenv virtualenvwrapper
+sudo pip install --upgrade pip setuptools
+sudo pip install awscli virtualenv virtualenvwrapper
