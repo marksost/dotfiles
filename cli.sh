f845d3061d4acfd748eb025598cde91f2cb5b26f
Mark Sost <sostmark@gmail.com>
2018-10-15 11:02:02 -0400

    Standard echo

--- cli.sh
+++ cli.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 # Install apt packages
-echo_yellow "Installing apt packages..."
+echo "Installing apt packages..."
 sudo apt update
 sudo apt install -y $(grep -vE "^\s*#" "./apt/packages.txt"  | tr "\n" " ")
 
 # Install NPM packages
-echo_yellow "Installing node packages..."
+echo "Installing node packages..."
 npm install -g eslint jshint
 
 # Install gems
-echo_yellow "Installing ruby gems..."
+echo "Installing ruby gems..."
 gem install github_changelog_generator
 gem install travis
 
 # Install PIP utilities
-echo_yellow "Installing pip packages..."
+echo "Installing pip packages..."
 sudo pip install --upgrade pip setuptools
 sudo pip install awscli virtualenv virtualenvwrapper
 
 # Install Terraform
-echo_yellow "Installing Terraform..."
+echo "Installing Terraform..."
 (
   cd /tmp
   wget https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
