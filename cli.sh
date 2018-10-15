4c77964895bf54b8c19093b0d2e16d7cc1ca6b84
Mark Sost <sostmark@gmail.com>
2018-10-15 10:36:08 -0400

    Updates

--- cli.sh
+++ cli.sh
@@ -1,32 +1,26 @@
 #!/usr/bin/env bash
 
 # Install RVM before installing other binaries
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable --ruby
-rvm install 2.4.1
-rvm use 2.4.1
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+# curl -sSL https://get.rvm.io | bash -s stable --ruby
+# rvm install 2.4.1
+# rvm use 2.4.1
 
-# Install brew packages
-# for FORMULA in $(cat "./brew/formula-list.txt"); do
-#   brew install "${FORMULA}"
-# done
+# Install apt packages
+# apt install -y $(grep -vE "^\s*#" "./apt/packages.txt"  | tr "\n" " ")
 
 # Install NPM packages
-npm install -g eslint jshint bower grunt-cli
+# npm install -g eslint jshint bower grunt-cli
 
 # Install gems
-gem install github_changelog_generator
-gem install travis
+# gem install github_changelog_generator
+# gem install travis
 
 # Get and install PIP
-cd /tmp
-curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
+# cd /tmp
+# curl -O https://bootstrap.pypa.io/get-pip.py
+# python get-pip.py
 
 # Install PIP utilities
-pip install --upgrade pip setuptools
-pip install awscli virtualenv virtualenvwrapper
-
-# Install GCP tools
-# NOTE: Interractive
-curl https://sdk.cloud.google.com | bash
+# pip install --upgrade pip setuptools
+# pip install awscli virtualenv virtualenvwrapper
